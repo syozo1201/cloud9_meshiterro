0e5d6d65e860953b1695568cb7a20590dd423185
syozo1201 <00syozo1201@gmail.com>
2021-01-21 04:17:30 +0000

    first commit

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1093ec2aac02181eed23c58a81448a2cb9a5e154c540e55d0eaf952a355912db885b37a57143a6362b511e105aee2767f9cc2d1b16ecf939ce22802945c929d2'
